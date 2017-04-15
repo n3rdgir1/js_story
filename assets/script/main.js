803562f134937eff54854493948b65e574e053a1
n3rdgir1 <nerdgirl11235813@gmail.com>
2017-04-15 17:11:39 -0500

    get to chapter 2

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $('#step1').append( `
     <p>
       $('.story').on('click', '#step1', function() {<br>
-      &nbsp;&nbsp;'#step2').css({'display': 'block'});<br>
+      &nbsp;&nbsp;$('#step2').css({'display': 'block'});<br>
       });<br>
     </p>`);
   $('.story').on('click', '#step1', function() {
@@ -20,12 +20,42 @@ $(document).ready(function(){
       });
     </p>`);
   $('.story').on('click', '#step2', function() {
-    var top = $('#step3').position().top;
-    $(window).scrollTop( top );
+    console.log('click');
+    $('.story').on('click', '#step2', function() {
+      var top = $('#step3').position().top;
+      $(window).scrollTop( top );
+      setTimeout(function() {
+        window.location = 'chapter2.html';
+      }, 20000)
+    });
   });
 
   $('#step3').append( `<p>
+    setTimeout(function() {<br>
+    &nbsp;&nbsp;window.location = 'chapter2.html';<br>
+    }, 20000)<br>
     </p>`);
-  $('.story').on('click', '#step3', function() {
+
+
+  $('#step4').append( `<p>
+    if( !$('#step4')[0].style['background-color'] ) {<br>
+    &nbsp;&nbsp;$('#step4').css( {'background-color': 'black', 'color': 'white'} );<br>
+    } <span>else {<br>
+    &nbsp;&nbsp;$('.story').append( step5 );<br>
+    }</span>
+    </p>`);
+
+  $('.story').on('click', '#step4', function() {
+    var step5 = `<div class=step id=step5>
+      <p>
+        content
+      </p>
+    </div>`
+    if(!$('#step4')[0].style['background-color']) {
+      $('#step4').css({'background-color': 'black', 'color': 'white'});
+    } else {
+      $('.story').append(step5);
+    }
   });
+
 });
